6650ce83f268f32787ee2a1298f2a672f47e326c
moktubat <shadhinreja@gmail.com>
2023-07-24 18:47:08 +0600

    single data starching

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.odqhq4i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,14 +26,15 @@ async function run() {
     client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("College admission running");
@@ -44,11 +43,17 @@ app.get("/", (req, res) => {
 const collegesCollection = client.db("collegeDb").collection("colleges");
 const usersCollection = client.db("collegeDb").collection("users");
 
+app.get("/colleges", async (req, res) => {
+  const result = await collegesCollection.find().toArray();
+  res.send(result);
+});
 
-app.get('/colleges', async(req, res) => {
-    const result = await collegesCollection.find().toArray();
-    res.send(result);
-})
+app.get("/collegeInfo/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await collegesCollection.findOne(query);
+  res.send(result);
+});
 
 app.get("/users", async (req, res) => {
   const result = await usersCollection.find().toArray();
